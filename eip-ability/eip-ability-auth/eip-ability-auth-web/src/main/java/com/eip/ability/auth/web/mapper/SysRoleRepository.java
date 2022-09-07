5d6c6dacd9f8092ea98e5a15dad78ee7943756a5
zhangliang102411@126.com <zhangliang102411@126.com>
2022-09-07 16:28:11 +0800

    feat(oauth2): 移除过期代码

--- eip-ability/eip-ability-auth/eip-ability-auth-web/src/main/java/com/eip/ability/auth/web/mapper/SysRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eip.ability.auth.web.mapper;
-
-import com.eip.ability.auth.web.model.po.SysRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SysRoleRepository  extends JpaRepository<SysRole,Long> {
-}
